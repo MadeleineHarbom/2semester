0261e1a5f9c98da8f1cced9070d48da46bb13259
Madeleine <madeleine.harbom@gmail.com>
2018-12-10 22:22:02 +0100

    aflevering named

--- Lektion 26 Søgetræer/src/opgave2/App.java
+++ Lektion 26 Søgetræer/src/opgave2/App.java
@@ -2,75 +2,74 @@ package opgave2;
 
 public class App {
     public static void main(String[] args) {
-        BinarySearchTree<Integer> weihnachtsSucheBaum = new BinarySearchTree<>();
-        weihnachtsSucheBaum.add(45);
-        weihnachtsSucheBaum.add(22);
-        weihnachtsSucheBaum.add(77);
-        weihnachtsSucheBaum.add(11);
-        weihnachtsSucheBaum.add(30);
-        weihnachtsSucheBaum.add(90);
-        weihnachtsSucheBaum.add(15);
-        weihnachtsSucheBaum.add(25);
-        weihnachtsSucheBaum.add(88);
+        BinarySearchTree<Integer> yggdrasil = new BinarySearchTree<>();
+        yggdrasil.add(45);
+        yggdrasil.add(22);
+        yggdrasil.add(77);
+        yggdrasil.add(11);
+        yggdrasil.add(30);
+        yggdrasil.add(90);
+        yggdrasil.add(15);
+        yggdrasil.add(25);
+        yggdrasil.add(88);
 
-        weihnachtsSucheBaum.print();
+        yggdrasil.print();
         System.out.println("Preorder");
-        System.out.println(weihnachtsSucheBaum.preorder());
+        System.out.println(yggdrasil.preorder());
         System.out.println();
         System.out.println("Postorder");
-        System.out.println(weihnachtsSucheBaum.postorder());
+        System.out.println(yggdrasil.postorder());
         System.out.println();
         System.out.println("Inorder");
-        System.out.println(weihnachtsSucheBaum.inorder());
+        System.out.println(yggdrasil.inorder());
         System.out.println();
-        System.out.println("Stoerste: " + weihnachtsSucheBaum.findMax());
-        System.out.println("Minste: " +weihnachtsSucheBaum.findMin());
-        weihnachtsSucheBaum.print();
+        System.out.println("Stoerste: " + yggdrasil.findMax());
+        System.out.println("Minste: " +yggdrasil.findMin());
+        yggdrasil.print();
         System.out.println("Remobve 30");
-        weihnachtsSucheBaum.remove(30);
-        weihnachtsSucheBaum.print();
+        yggdrasil.remove(30);
+        yggdrasil.print();
         System.out.println("Remove 88");
-        weihnachtsSucheBaum.remove(88);
-        weihnachtsSucheBaum.print();
+        yggdrasil.remove(88);
+        yggdrasil.print();
         System.out.println("Remove 45");
-        weihnachtsSucheBaum.remove(45);
-        weihnachtsSucheBaum.print();
+        yggdrasil.remove(45);
+        yggdrasil.print();
         System.out.println("Remove 80");
-        weihnachtsSucheBaum.remove(80);
-        weihnachtsSucheBaum.print();
+        yggdrasil.remove(80);
+        yggdrasil.print();
 
 
         System.out.println("Find min");
-        System.out.println(weihnachtsSucheBaum.findMin());
+        System.out.println(yggdrasil.findMin());
         System.out.println("Remove 90");
-        weihnachtsSucheBaum.remove(90);
-        weihnachtsSucheBaum.print();
+        yggdrasil.remove(90);
+        yggdrasil.print();
         System.out.println("Find max");
-        System.out.println(weihnachtsSucheBaum.findMax());
+        System.out.println(yggdrasil.findMax());
 
         System.out.println("-----NEW TREE-----");
-        BinarySearchTree<Integer> weihnachtsSucheBaum1 = new BinarySearchTree<>();
-        weihnachtsSucheBaum1.add(45);
-        weihnachtsSucheBaum1.add(22);
-        weihnachtsSucheBaum1.add(77);
-        weihnachtsSucheBaum1.add(11);
-        weihnachtsSucheBaum1.add(30);
-        weihnachtsSucheBaum1.add(90);
-        weihnachtsSucheBaum1.add(15);
-        weihnachtsSucheBaum1.add(25);
-        weihnachtsSucheBaum1.add(88);
+        BinarySearchTree<Integer> treeBeard = new BinarySearchTree<>();
+        treeBeard.add(45);
+        treeBeard.add(22);
+        treeBeard.add(77);
+        treeBeard.add(11);
+        treeBeard.add(30);
+        treeBeard.add(90);
+        treeBeard.add(15);
+        treeBeard.add(25);
+        treeBeard.add(88);
 
-        weihnachtsSucheBaum1.print();
-        System.out.println("Remove min " + weihnachtsSucheBaum1.removeMin());
-        //weihnachtsSucheBaum1.removeMin();
-        weihnachtsSucheBaum1.print();
+        treeBeard.print();
+        System.out.println("Remove min " + treeBeard.removeMin());
+        treeBeard.print();
 
-        System.out.println("Remove min " +  weihnachtsSucheBaum1.removeMin());
-        //weihnachtsSucheBaum1.removeMin();
-        weihnachtsSucheBaum1.print();
-        System.out.println("Remove min " +  weihnachtsSucheBaum1.removeMin());
-        //weihnachtsSucheBaum1.removeMin();
-        weihnachtsSucheBaum1.print();
+        System.out.println("Remove min " +  treeBeard.removeMin());
+
+        treeBeard.print();
+        System.out.println("Remove min " +  treeBeard.removeMin());
+
+        treeBeard.print();
 
 
         System.out.println("-----");
